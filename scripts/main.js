8935c84e6adf6a8511afb4119fd66f472de5a3ff
Anselmo Diabaca <anselmo23diababa@gmail.com>
2023-06-15 17:46:23 +0000

    Fix: Resumo

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,29 @@ function digitarLetras(elemento) {
 
 const titulo = document.querySelector('.digitando');
 digitarLetras(titulo);
+
+//ESSE SCRIPT TRAZ O MENO E O ESCONDE
+const ativaMenu = document.querySelector('.fa-bars');
+const listMenu = document.querySelector('header .list-menu')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    listMenu.classList.toggle('ativado')
+})
+
+//ESSE SCRIPT TRAZ O RESUMO
+const divExperienc = document.querySelectorAll('.esperiencia-content .feito');
+const listExperienc = document.querySelectorAll('.esperiencia-content .list-span li')
+
+const divEducation = document.querySelectorAll('.educacao-content .feito');
+const listEducation = document.querySelectorAll('.educacao-content .list-span li')
+
+function slidShow(index) {
+    divExperienc.forEach((feito) =>{
+        feito.classList.remove('ativo')
+    });
+    divEducation.forEach((botao) =>{
+        botao.classList.remove('ativo')
+    });
+    divExperienc[index].classList.add('ativo')
+}
